2aec7c9460b6ad914232aff28ed4a6f78d9a2168
duerotkd <dureotkd123@naver.com>
2022-02-02 23:22:25 +0900

    finish

--- src/helper/port.js
+++ src/helper/port.js
@@ -1,10 +1,10 @@
 const baseServerUrl =
-  window.location.host.indexOf("localhost") == -1
+  window.location.host.indexOf("localhost") !== -1
     ? "http://localhost:8080"
     : "http://www.lolbick.com:8080";
 
 const baseUrl =
-  window.location.host.indexOf("localhost") == -1
+  window.location.host.indexOf("localhost") !== -1
     ? "http://localhost:3000"
     : "http://www.lolbick.com";
 
