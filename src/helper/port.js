9bf1353209a7da84d94ac17fc92decfe0bc3cc39
duerotkd <dureotkd123@naver.com>
2022-02-02 18:16:09 +0900

    test

--- src/helper/port.js
+++ src/helper/port.js
@@ -1,7 +1,7 @@
 const baseServerUrl =
   window.location.host.indexOf("localhost") !== -1
     ? "http://localhost:80"
-    : "http://13.209.99.30:80";
+    : "http://13.209.99.30:8080";
 
 const baseUrl =
   window.location.host.indexOf("localhost") !== -1
