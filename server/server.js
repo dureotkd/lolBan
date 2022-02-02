6611f4fffd3af354cb9aba97c428a29480b7503d
duerotkd <dureotkd123@naver.com>
2022-02-02 17:31:03 +0900

    a

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ const { http } = require("./http/http");
 require("./api/api");
 require("./socket/socket");
 
-http.listen(8080, (req, res) => {
+http.listen(80, (req, res) => {
   console.log(`서버를 요청 받을 준비가 되었습니다 👩`);
 });
