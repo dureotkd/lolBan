7d56130d18834e4fb82e6311b0c04268c2874930
성민 <63378895+dureotkd@users.noreply.github.com>
2024-07-05 11:41:39 +0900

    qwf

--- src/helper/port.js
+++ src/helper/port.js
@@ -1,11 +1,11 @@
 const baseServerUrl =
   window.location.host.indexOf("localhost") !== -1
     ? "http://localhost:8080"
-    : "http://www.lolbick.com:8080";
+    : "http://211.238.133.10:8080";
 
 const baseUrl =
   window.location.host.indexOf("localhost") !== -1
     ? "http://localhost:3000"
-    : "http://www.lolbick.com";
+    : "http://211.238.133.10";
 
 export { baseServerUrl, baseUrl };
